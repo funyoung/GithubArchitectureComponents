8c3a32816e75419715ca13a97c024a0f4226173c
yangfeng <yangfeng@sogou-inc.com>
2020-05-07 16:49:57 +0800

    circle header scrolling.

--- app/src/main/java/com/boisneyphilippe/githubarchitecturecomponents/ui/fragments/UserProfileFragment.java
+++ app/src/main/java/com/boisneyphilippe/githubarchitecturecomponents/ui/fragments/UserProfileFragment.java
@@ -117,6 +117,11 @@ public class UserProfileFragment extends Fragment implements AppBarLayout.OnOffs
 
     @Override
     public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+        int maxAlpha = 255;
+        int r = Color.red(Color.DKGRAY);
+        int g = Color.green(Color.DKGRAY);
+        int b = Color.blue(Color.DKGRAY);
+
         //垂直方向偏移量
         int offset = Math.abs(verticalOffset);
         //最大偏移距离
@@ -126,18 +131,18 @@ public class UserProfileFragment extends Fragment implements AppBarLayout.OnOffs
             toolbarClose.setVisibility(View.GONE);
             //根据偏移百分比 计算透明值
             float scale2 = (float) offset / (scrollRange / 2);
-            int alpha2 = (int) (255 * scale2);
-            bgToolbarOpen.setBackgroundColor(Color.argb(alpha2, 25, 131, 209));
+            int alpha2 = (int) (maxAlpha * scale2);
+            bgToolbarOpen.setBackgroundColor(Color.argb(alpha2, r, g, b));
         } else {//当滑动超过一半，收缩状态下toolbar显示内容，根据收缩位置，改变透明值
             toolbarClose.setVisibility(View.VISIBLE);
             toolbarOpen.setVisibility(View.GONE);
             float scale3 = (float) (scrollRange  - offset) / (scrollRange / 2);
-            int alpha3 = (int) (255 * scale3);
-            bgToolbarClose.setBackgroundColor(Color.argb(alpha3, 25, 131, 209));
+            int alpha3 = (int) (maxAlpha * scale3);
+            bgToolbarClose.setBackgroundColor(Color.argb(alpha3, r, b, b));
         }
         //根据偏移百分比计算扫一扫布局的透明度值
         float scale = (float) offset / scrollRange;
-        int alpha = (int) (255 * scale);
-        bgContent.setBackgroundColor(Color.argb(alpha, 25, 131, 209));
+        int alpha = (int) (maxAlpha * scale);
+        bgContent.setBackgroundColor(Color.argb(alpha, r, g, b));
     }
 }
